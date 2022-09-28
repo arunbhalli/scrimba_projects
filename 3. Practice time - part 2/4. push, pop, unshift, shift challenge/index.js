0adbfe684e82ec5a6869c3e3af79ec52adafe725
arunbhalli <tomarbhalli@gmail.com>
2022-09-28 18:37:11 +0200

    adds example of changing values

---
+++ 3. Practice time - part 2/4. push, pop, unshift, shift challenge/index.js
@@ -0,0 +1,10 @@
+// You need to help me fixup the largeCountries array so that
+// China and Pakistan are added back into their respective places
+
+// Use push() & pop() and their counterparts unshift() & shift()
+// Google how to use unshift() and shift()
+let largeCountries = ['Tuvalu', 'India', 'USA', 'Indonesia', 'Monaco'];
+largeCountries[0] = 'China';
+largeCountries[4] = 'Pakistan';
+console.log(largeCountries[0]);
+console.log(largeCountries);
