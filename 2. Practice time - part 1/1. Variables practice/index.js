48112b2118bb30743ea6a0e8ffab915e4527f197
arunbhalli <tomarbhalli@gmail.com>
2022-09-28 15:02:04 +0200

    adds variables practice

---
+++ 2. Practice time - part 1/1. Variables practice/index.js
@@ -0,0 +1,11 @@
+// Create two variables, firstName and lastName
+
+// Concatenate the two variables into a third variable called fullName
+
+// Log fullName to the console
+
+let firstName = 'Arun';
+let lastName = 'Singh';
+console.log(firstName + ' ' + lastName);
+let fullNmae = firstName.concat(lastName);
+console.log(fullNmae);
